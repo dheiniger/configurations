bb2af293e97126ad7a5b83beb4a7c50e3e424d19
Daniel Heiniger <daniel.r.heiniger@gmail.com>
2018-12-23 21:41:53 -0600

    Initial emacs configuration

---
+++ init.el
@@ -0,0 +1,23 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(ido-mode 1)
+(setq ido-enable-flex-matching t)
+(setq inhibit-startup-message t
+inhibit-startup-echo-area-message t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "light steel blue" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "outline" :family "Courier New")))))
