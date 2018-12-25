862133dfbe7fc4792df19055a95318a6b2a186cd
Daniel Heiniger <daniel.r.heiniger@gmail.com>
2018-12-25 13:26:08 -0600

    change emacs theme

--- init.el
+++ init.el
@@ -16,10 +16,11 @@ inhibit-startup-echo-area-message t)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (deeper-blue)))
  '(package-selected-packages (quote (clojure-mode-extra-font-locking cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "light slate gray" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "outline" :family "Courier New")))))
+ )
